7936a848231551a0a0402440251635217077eae6
StanislavJS <stasyk55@gmail.com>
2025-08-01 18:46:50 +0300

    update app

--- src/app/notes/page.tsx
+++
@@ -1,9 +0,0 @@
-import NotesList from "@/components/NoteList/NoteList";
-
-export default function NotesPage() {
-  return (
-    <main className="p-8">
-      <NotesList notes={[]} />
-    </main>
-  );
-}
